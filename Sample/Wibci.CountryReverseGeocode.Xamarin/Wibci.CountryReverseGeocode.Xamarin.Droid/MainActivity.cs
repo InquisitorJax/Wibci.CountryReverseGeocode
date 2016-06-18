423eaa27b8dd510310c79a90e06817038c7dd610
Malcolm Jack <inquisitorjax@gmail.com>
2016-06-18 20:44:46 +0200

    initial cross platform sample project (iOS, Android, UWP)

---
+++ Sample/Wibci.CountryReverseGeocode.Xamarin/Wibci.CountryReverseGeocode.Xamarin.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Wibci.CountryReverseGeocode.Xamarin;
+
+namespace Wibci.CountryReverseGeocode.Xamarin.Droid
+{
+    [Activity(Label = "Wibci.CountryReverseGeocode.Xamarin", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
