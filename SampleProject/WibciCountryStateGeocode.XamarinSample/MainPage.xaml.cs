8c0f482a3ab2b52245970c9d2f1e22c383517cd3
InquisitorJax <inquisitorjax@gmail.com>
2020-06-20 14:11:51 +0200

    update to fix cali + nuget publish

---
+++ SampleProject/WibciCountryStateGeocode.XamarinSample/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace WibciCountryStateGeocode.XamarinSample
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			BindingContext = new MainViewModel();
+		}
+	}
+}
---
+++ SampleProject/WibciCountryStateGeocode.XamarinSample/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows.Input;
+using Wibci.CountryReverseGeocode;
+using Wibci.CountryReverseGeocode.Models;
+
+namespace WibciCountryStateGeocode.XamarinSample
+{
+	public class MainViewModel : BindableBase
+    {
+        private string _countryResult;
+        private ICountryReverseGeocodeService _geocodeService;
+
+        private string _stateResult;
+
+        public MainViewModel()
+        {
+            GeocodeCommand = new DelegateCommand(Geocode);
+            _geocodeService = new CountryReverseGeocodeService();
+        }
+
+        public string CountryResult
+        {
+            get { return _countryResult; }
+            set { SetProperty(ref _countryResult, value); }
+        }
+
+        public ICommand GeocodeCommand {  get; private set; }
+
+		private string _latitude;
+
+		public string Latitude
+		{
+			get { return _latitude; }
+			set { SetProperty(ref _latitude, value); }
+		}
+
+		private string _longitude;
+
+		public string Longitude
+		{
+			get { return _longitude; }
+			set { SetProperty(ref _longitude, value); }
+		}
+
+
+		public string StateResult
+        {
+            get { return _stateResult; }
+            set { SetProperty(ref _stateResult, value); }
+        }
+
+        private void Geocode()
+        {
+            bool latSuccess = double.TryParse(Latitude, out double lat);
+            bool lonSuccess = double.TryParse(Longitude, out double lon);
+
+            if (latSuccess && lonSuccess)
+            {
+                GeoLocation location = new GeoLocation { Latitude = lat, Longitude = lon };
+
+                var countryResult = _geocodeService.FindCountry(location);
+                var stateResult = _geocodeService.FindUsaState(location);
+
+                CountryResult = countryResult?.Name ?? "Error";
+                StateResult = stateResult?.Name ?? "Error";
+            }
+        }
+    }
+}
